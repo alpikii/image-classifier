6c3135fba61dd8f05b6dbff517c2918eb676b713
alpikii <98106894+alpikii@users.noreply.github.com>
2025-07-20 15:46:24 +0000

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=project_2013056
+#SBATCH --ntasks=1
+#SBATCH --nodes=1  
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16g
+#SBATCH --time=04:00:00
+#SBATCH --partition=small
+
+module load pytorch
+srun python3 -u /scratch/project_2013056/train.py
